84466236d30e11657c0ae3259ec595f9caabab0f
Gregor <Grigorjan>
2021-11-25 11:39:19 +0200

    --amend

--- JustParseThePDF/OCR/ImageTextExtractor.cs
+++ JustParseThePDF/OCR/ImageTextExtractor.cs
@@ -6,7 +6,6 @@ namespace gekoke.JustParse.OCR {
         /// Extract the text from the given image using optical character recognition.
         /// </summary>
         /// <param name="sourceFilePath">The image to extract text from</param>
-        /// <param name="dpi">The resolution to use when scanning the image for text (higher is more accurate but slower - default is 500)</param>
         /// <returns>The text as parsed from the input image</returns>
         public static string GetImageText(
             string sourceFilePath,
